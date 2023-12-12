604fc5ce6786fc9ef05dcd60d36f215b09873754
Ron Boortman <ron@boortman.nl>
2023-12-12 15:49:15 +0100

    Update day_12.py

--- 2023/src/solutions/day_12.py
+++ 2023/src/solutions/day_12.py
@@ -1,5 +1,7 @@
 from enum import Enum
+from itertools import permutations
 from .solution import Solution
+
 
 class Spring(Enum):
     OPERATIONAL = "."
@@ -11,6 +13,7 @@ class Spring(Enum):
 
     def __repr__(self) -> str:
         return self.__str__()
+
 
 class SpringRow:
     def __init__(self, row_str: str) -> None:
@@ -32,14 +35,13 @@ class SpringRow:
             groups.append(current_group)
         return groups
 
-
     def get_arrangements(self) -> int:
         groups = self.get_groups()
-        if len(groups) == 0:
-            return 1
-        
-        to_check = self.damaged_sizes[:]
         arrangements = 1
+        if len(groups) == 0:
+            return arrangements
+
+        to_check = self.damaged_sizes[:]
         while len(to_check) > 0:
             can_break = False
 
@@ -65,7 +67,76 @@ class SpringRow:
         if len(groups) == 1 and (sum(to_check) + len(to_check) - 1) == len(groups[0]):
             return arrangements
 
+        if len(groups) == len(to_check):
+            sub_arrangements = 1
+            print(groups, to_check)
+            for i, group in enumerate(groups):
+                if len(group) != to_check[i]:
+                    try:
+                        first_part = group.index(Spring.BROKEN)
+                        last_part = max(
+                            i for i, val in enumerate(group) if val == Spring.BROKEN
+                        )
+                        sub_arrangements *= to_check[i] - (last_part - first_part)
+                    except ValueError:
+                        sub_arrangements *= len(group) - (to_check[i] - 1)
+            return sub_arrangements
+
         return arrangements - 1
+
+    def is_valid_combination(self, combination: list[Spring]) -> bool:
+        to_check = self.damaged_sizes[:]
+        broken_size = 0
+        for i, spring in enumerate(combination):
+            if self.springs[i] != Spring.UNKNOWN and self.springs[i] != spring:
+                return False
+
+            if spring == Spring.BROKEN:
+                broken_size += 1
+                if broken_size > to_check[0]:
+                    return False
+
+            else:
+                if broken_size <= 0:
+                    continue
+                if broken_size == to_check[0]:
+                    to_check.pop(0)
+                    broken_size = 0
+                else:
+                    return False
+
+        return True
+
+    def get_all_combinations(self) -> list[list[Spring]]:
+        total_broken = sum([1 for spring in self.springs if spring == Spring.BROKEN])
+        total_unknown = sum([1 for spring in self.springs if spring == Spring.UNKNOWN])
+        all_broken = [
+            Spring.BROKEN for _ in range(sum(self.damaged_sizes) - total_broken)
+        ]
+        all_operational = [
+            Spring.OPERATIONAL for _ in range(total_unknown - len(all_broken))
+        ]
+        perm = permutations(all_broken + all_operational)
+
+        unknown_indices = [
+            i for i, spring in enumerate(self.springs) if spring == Spring.UNKNOWN
+        ]
+        valid_combinations = []
+        checked_combinations = dict()
+        for p in perm:
+            if p in checked_combinations:
+                continue
+            checked_combinations[p] = True
+
+            combination = self.springs[:]
+            for i, index in enumerate(unknown_indices):
+                combination[index] = p[i]
+            if self.is_valid_combination(combination):
+                valid_combinations.append(combination)
+        return len(valid_combinations)
+
+    def get_bruteforce_arrangements(self) -> int:
+        groups = self.get_groups()
 
     def __str__(self) -> str:
         return f"{''.join([str(s) for s in self.springs])} {','.join([str(d) for d in self.damaged_sizes])}"
@@ -77,12 +148,14 @@ class SpringRow:
 class Assignment(Solution):
     def parse_input(self, input: str, is_gold: bool = False) -> list[SpringRow]:
         return [SpringRow(row) for row in input.split("\n")]
-    
+
     def silver(self, input: list[SpringRow]) -> int:
-        print(input)
-        for row in input:
-            print(row.get_arrangements())
-        return 0
-    
+        s = 0
+        for i, row in enumerate(input):
+            print(i)
+            s += row.get_all_combinations()
+        return s
+        # return sum([row.get_all_combinations() for row in input])
+
     def gold(self, input: list[SpringRow]) -> int:
         return 0
