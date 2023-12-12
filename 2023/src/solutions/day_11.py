3f6b58d94bb6a7d70fede053452377d975efc5f5
Ron Boortman <ron@boortman.nl>
2023-12-12 06:18:51 +0100

    Finishes day 11

--- 2023/src/solutions/day_11.py
+++ 2023/src/solutions/day_11.py
@@ -52,6 +52,23 @@ class Space:
         self.galaxies = self.find_galaxies()
         return self
 
+    def mock_expand(self, size: int = 1) -> list[tuple[int, int]]:
+        galaxy_x = [galaxy[0] for galaxy in self.galaxies]
+        galaxy_y = [galaxy[1] for galaxy in self.galaxies]
+
+        empty_columns = [i for i in range(len(self.grid[0])) if i not in galaxy_x]
+        empty_rows = [i for i in range(len(self.grid)) if i not in galaxy_y]
+
+        new_galaxies = []
+
+        for galaxy in self.galaxies:
+            x_empty = len([x for x in empty_columns if x < galaxy[0]])
+            y_empty = len([y for y in empty_rows if y < galaxy[1]])
+
+            new_galaxies.append((galaxy[0] + (x_empty * (size - 1)), galaxy[1] + (y_empty * (size - 1))))
+
+        return new_galaxies
+
     def __str__(self) -> str:
         return "\n".join(["".join([str(cell) for cell in row]) for row in self.grid])
 
@@ -74,11 +91,11 @@ class Assignment(Solution):
         )
 
     def gold(self, input: Space) -> int:
-        input.expand(10)
+        mock_galaxies = input.mock_expand(1_000_000)
         return sum(
             [
                 abs(galaxy_a[0] - galaxy_b[0]) + abs(galaxy_a[1] - galaxy_b[1])
-                for (i, galaxy_a) in enumerate(input.galaxies)
-                for galaxy_b in input.galaxies[i + 1 :]
+                for (i, galaxy_a) in enumerate(mock_galaxies)
+                for galaxy_b in mock_galaxies[i + 1 :]
             ]
         )
