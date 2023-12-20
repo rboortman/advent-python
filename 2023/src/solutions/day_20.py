078765ec84d660ee7947ba06a3a4f9e165e01a9e
Ron Boortman <ron@boortman.nl>
2023-12-20 11:14:37 +0100

    Update day_20.py

--- 2023/src/solutions/day_20.py
+++ 2023/src/solutions/day_20.py
@@ -1,3 +1,4 @@
+import math
 from enum import Enum
 from .solution import Solution
 
@@ -5,48 +6,38 @@ from .solution import Solution
 class Signal(Enum):
     HIGH = 1
     LOW = 0
+    FINISHED = -1
 
     def __str__(self) -> str:
         return str(f"-{self.name}->")
-    
+
     def __repr__(self) -> str:
         return self.__str__()
-    
-    def __invert__(self) -> "Signal":
-        return Signal(1 - self.value)
-    
-    def __and__(self, other: "Signal") -> "Signal":
-        return Signal(self.value & other.value)
-    
-    def __or__(self, other: "Signal") -> "Signal":
-        return Signal(self.value | other.value)
-    
-    def __xor__(self, other: "Signal") -> "Signal":
-        return Signal(self.value ^ other.value)
-    
+
+
 class ModuleType(Enum):
     BUTTON = "button"
     BROADCASTER = "broadcaster"
     FLIP_FLOP = "%"
     CONJUNCTION = "&"
+    OUTPUT = "^"
+
 
 class Module:
     def __init__(self, input: str) -> None:
         input_output = input.split(" -> ")
+
+        self.id = input_output[0]
+        self.input = []
+        self.output = input_output[1].split(", ")
+
         if input_output[0] == ModuleType.BUTTON.value:
-            self.id = input_output[0]
-            self.input = []
             self.type = ModuleType.BUTTON
-            self.output = input_output[1].split(", ")
         elif input_output[0] == ModuleType.BROADCASTER.value:
-            self.id = input_output[0]
-            self.input = []
             self.type = ModuleType.BROADCASTER
-            self.output = input_output[1].split(", ")
         else:
             self.id = input_output[0][1:]
             self.type = ModuleType(input_output[0][0])
-            self.input = []
             self.output = input_output[1].split(", ")
 
         if self.type == ModuleType.FLIP_FLOP:
@@ -65,38 +56,79 @@ class Module:
     def process_signal(self, signal: Signal, source: str) -> (Signal, list[str]):
         out_signal = None
         if self.type == ModuleType.BUTTON or self.type == ModuleType.BROADCASTER:
-            out_signal = signal
+            out_signal = Signal(signal.value)
         elif self.type == ModuleType.FLIP_FLOP:
             if signal == Signal.LOW:
                 self.on = not self.on
                 out_signal = Signal(self.on)
         elif self.type == ModuleType.CONJUNCTION:
             self.memory[source] = signal
-            if all(self.memory.values()):
+            if all([s.value for s in self.memory.values()]):
                 out_signal = Signal.LOW
             else:
                 out_signal = Signal.HIGH
+        elif self.type == ModuleType.OUTPUT and signal == Signal.LOW:
+            out_signal = Signal.FINISHED
 
         return out_signal, self.output
 
     def __str__(self) -> str:
         return f"{self.id} -> {self.output}"
-    
+
     def __repr__(self) -> str:
         return self.__str__()
 
 
-
 class Assignment(Solution):
     def parse_input(self, input: str, is_gold: bool = False) -> dict[str, Module]:
-        input += "\nbutton -> broadcaster"
+        # input += "\nbutton -> broadcaster"
+        input += "\n^output -> output"
+        input += "\n^rx -> rx"
         modules = [Module(m) for m in input.splitlines()]
         modules_dict = {m.id: m for m in modules}
-        return 
-    
+        for module in modules:
+            for output in module.output:
+                modules_dict[output].add_input([module.id])
+        return modules_dict
+
     def silver(self, input: dict[str, Module]) -> int:
-        print(input)
-        return 0
-    
+        # print(input)
+        button_pressed = 1000
+        signal_count = [0, 0]
+        for _ in range(button_pressed):
+            signal_backlog = [("button", Signal.LOW, "broadcaster")]
+            while len(signal_backlog) > 0:
+                source, signal, dest = signal_backlog.pop(0)
+                # print(f"{signal} {dest}")
+                signal_count[signal.value] += 1
+                out_signal, output = input[dest].process_signal(signal, source)
+                if out_signal is not None and out_signal != Signal.FINISHED:
+                    for o in output:
+                        # print(f"{dest} {out_signal} {o}")
+                        signal_backlog.append((dest, out_signal, o))
+        # print(signal_count)
+        return math.prod(signal_count)
+
     def gold(self, input: dict[str, Module]) -> int:
-        return 0
+        if "rx" not in input:
+            return 0
+
+        # print(input)
+        button_pressed = 0
+        while True:
+            button_pressed += 1
+
+            if button_pressed % 100_000 == 0:
+                print(button_pressed)
+
+            signal_backlog = [("button", Signal.LOW, "broadcaster")]
+            while len(signal_backlog) > 0:
+                source, signal, dest = signal_backlog.pop(0)
+                # print(f"{signal} {dest}")
+                out_signal, output = input[dest].process_signal(signal, source)
+                if out_signal == Signal.FINISHED:
+                    return button_pressed
+                if out_signal is not None and out_signal != Signal.FINISHED:
+                    for o in output:
+                        # print(f"{dest} {out_signal} {o}")
+                        signal_backlog.append((dest, out_signal, o))
